033783bd1393090180f6cc380ed188c118e2a7d1
Prasad-mutnale <prasadmutnale1234@gmail.com>
2023-06-15 19:53:52 +0530

    Button Added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Home.css'
+import Button from '@mui/material/Button';
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import AddIcon from '@mui/icons-material/Add';
+const Home = () => {
+  return (
+    <>
+        <div className='main'>
+            <div className='container'>
+                <p className='title'>Daily Sleep Tracker</p>
+
+                <div className='add-btn'>
+                   <div> <Button variant="contained" className="new-btn" startIcon={<AddIcon fontSize="large" />} sx={{
+                    backgroundColor:'#5795FA',
+    borderRadius: 50
+  }}>New Entry</Button></div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Home
